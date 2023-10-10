8fcf4ba0f9e6c7d3ce9ecea833e0932328fe7d50
tyu-hi <tyugo2122@gmail.com>
2023-10-10 15:02:29 -0700

    initial commit

---
+++ me/src/App.tsx
@@ -0,0 +1,35 @@
+/*import { useState } from 'react'*/
+import './App.css'
+
+function App() {
+  /*const [count, setCount] = useState(0)*/
+
+  return (
+
+    <html>
+      <head>
+        <title>Tingyu Gong</title>
+        <style>color: white</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </head>
+      <header className = "header">
+      <h1>Hello, my name is Tingyu!</h1>
+      </header>
+      <div className="card">
+        <h2>ABOUT ME</h2>
+        <p>
+          I am a 2nd-year Computer Science student at UCLA.</p> 
+          <p>There's nothing much yet, but it's honest work. I will add more to this website as I learn!
+        </p>
+        </div>
+        <h3>CONTACT</h3>
+        <p>tyugo2122@gmail.com</p>
+        <p><a href = "https://www.linkedin.com/in/tingyu-gong-330398253/">LinkedIn</a></p>
+        
+      
+
+    </html>
+  )
+}
+
+export default App
